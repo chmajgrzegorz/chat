d468e0328bc29a5b2c61bcb17444c8044b92dfd4
Your NameGrzegorz <chmajg@gmail.com>
2018-04-07 17:38:52 +0200

    Added historyMessages and MessagesLimit

--- src/main/java/pl/grzegorzchmaj/chat/models/ChatSocket.java
+++ src/main/java/pl/grzegorzchmaj/chat/models/ChatSocket.java
@@ -11,7 +11,9 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableWebSocket
 @Component
@@ -19,6 +21,7 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
 
     List<UserChatModel> userList = new ArrayList<>();
+    List<String> messageList = new ArrayList<>();
 
 
 
@@ -31,25 +34,73 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
         userList.add(new UserChatModel(session));
 
         UserChatModel userChatModel = findUserBySessionId(session);
+
         userChatModel.sendMessage("Witaj na naszym chacie");
         userChatModel.sendMessage("Wpisz swój nick");
+
+        for (String s : messageList) {
+            userChatModel.sendMessage(s);
+        }
+
+
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         UserChatModel userChatModel = findUserBySessionId(session);
-
-        if(userChatModel.getNickname() == null) {
-            userChatModel.setNickname(message.getPayload());
-            userChatModel.sendMessage("Ustawiono Twój nick!");
+        if(message.getPayloadLength() == 0){
+            userChatModel.sendMessage("Wpisz cokolwiek!");
             return;
         }
 
+        if(userChatModel.getNickname() == null) {
+            if(getNicknames().contains(message.getPayload())){
+                userChatModel.sendMessage("Nick jest zajęty");
+                userChatModel.sendMessage("Wpisz swój nick");
+            }
+            else{
+                userChatModel.setNickname(message.getPayload());
+                userChatModel.sendMessage("Ustawiono Twój nick!");
+            }
+
+            return;
+        }
+
+
+
+        if(userChatModel.getCounter() == 0) {
+            userChatModel.setTime(System.currentTimeMillis());
+            userChatModel.setCounter(userChatModel.getCounter() + 1);
+        }
+        else if(System.currentTimeMillis() - userChatModel.getTime() < 60000 ){
+            if(userChatModel.getCounter() < 29)
+                userChatModel.setCounter(userChatModel.getCounter() + 1);
+            else {
+                userChatModel.sendMessage("Nie mozesz wysylac wiadomosci");
+                return;
+            }
+        }
+        else{
+            userChatModel.setCounter(0);
+            userChatModel.setTime(System.currentTimeMillis());
+        }
+
+
+        if(messageList.size() == 30){
+            messageList.remove(0);
+            messageList.add(userChatModel.getNickname() + ": " + message.getPayload());
+            sendMessageToAll(userChatModel.getNickname() + ": " + message.getPayload());
+            return;
+        }
+        messageList.add(userChatModel.getNickname() + ": " + message.getPayload());
         sendMessageToAll(userChatModel.getNickname() + ": " + message.getPayload());
     }
+
+
 
     private void sendMessageToAll(String message) throws IOException {
         for (UserChatModel userChatModel : userList) {
@@ -65,4 +116,11 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
     private UserChatModel findUserBySessionId(WebSocketSession session){
         return userList.stream().filter(s -> s.getSession().equals(session)).findAny().get();
     }
+
+    private List<String> getNicknames(){
+        return userList.stream().map(s -> s.getNickname()).collect(Collectors.toList());
+    }
+
+    
+
 }
--- src/main/java/pl/grzegorzchmaj/chat/models/UserChatModel.java
+++ src/main/java/pl/grzegorzchmaj/chat/models/UserChatModel.java
@@ -8,13 +8,21 @@ import org.springframework.web.socket.WebSocketSession;
 import java.io.IOException;
 
 @Data
+@NoArgsConstructor
 public class UserChatModel {
 
     private WebSocketSession session;
     private String nickname;
+    private int counter;
+    private long time;
 
     public UserChatModel(WebSocketSession session) {
         this.session = session;
+        this.counter = 0;
+    }
+
+    public UserChatModel(String nickname) {
+        this.nickname = nickname;
     }
 
     public void sendMessage(String message) throws IOException {
