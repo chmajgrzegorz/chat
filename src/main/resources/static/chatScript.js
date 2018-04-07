29ba647f48b50074d7f406e6e8825ff202ccb627
Your NameGrzegorz <chmajg@gmail.com>
2018-04-07 12:46:40 +0200

    Added chatScript

---
+++ src/main/resources/static/chatScript.js
@@ -0,0 +1,27 @@
+function onLoad(){
+    var ws = "ws://localhost:8080/chat"
+    websocket = new WebSocket(ws);
+    websocket.onopen = function (ev) {
+        onopen(ev)
+    };
+    websocket.onmessage = function (ev) {
+        onmessage(ev);
+    }
+}
+function onOpen(){
+    console.log("Połączono")
+}
+
+function onMessage(ev){
+    var message = ev.data;
+
+    messages.innerHTML = messages.innerHTML + "<li class = \"message\">" + message + "</li>";
+    messages.scrollTop = messages.scrollHeight;
+}
+
+function sendMessage(){
+    var message = writer.value;
+    writer.value = "";
+
+    websocket.send(message);
+}
